ce2475766768f32958446b6a9785ab72121829de
Tim Redick <t.redick@irt.rwth-aachen.de>
2022-02-04 10:25:51 +0100

    set_context! with modifying convention

--- src/RenderContexts.jl
+++ src/RenderContexts.jl
@@ -3,10 +3,10 @@
 # All rights reserved.
 
 """
-    set_context(window)
-Set the context as current in GLFW and GLAbstractions
+    set_context!(window)
+Use this context for the current thread.
 """
-function set_context(window::GLFW.Window)
+function set_context!(window::GLFW.Window)
     GLFW.MakeContextCurrent(window)
     GLAbstraction.set_context!(window)
     return window
@@ -16,25 +16,25 @@ end
 const default_window_hints = [
     (GLFW.RESIZABLE, false),
     (GLFW.FOCUSED, false)
-    ]
+]
 
 """
     context_fullscreen(width, height; name, window_hints)
 Create an OpenGL context in fullscreen mode and makes it current.
 """
-context_fullscreen(width::Integer, height::Integer; name="SciGL.jl", window_hints=default_window_hints) = GLFW.Window(name=name, resolution=(width, height), windowhints=window_hints, fullscreen=true) |> set_context
+context_fullscreen(width::Integer, height::Integer; name = "SciGL.jl", window_hints = default_window_hints) = GLFW.Window(name = name, resolution = (width, height), windowhints = window_hints, fullscreen = true) |> set_context!
 
 """
     context_offscreen(width, height; name, window_hints)
 Create an OpenGL context which is not visible, e.g. for offscreen rendering and makes it current.
 """
-context_offscreen(width::Integer, height::Integer; name="SciGL.jl", window_hints=default_window_hints) = GLFW.Window(name=name, resolution=(width, height), windowhints=window_hints, visible=false) |> set_context
+context_offscreen(width::Integer, height::Integer; name = "SciGL.jl", window_hints = default_window_hints) = GLFW.Window(name = name, resolution = (width, height), windowhints = window_hints, visible = false) |> set_context!
 
 """
     context_window(width, height; name, window_hints)
 Create an OpenGL context in windowed mode and makes it current.
 """
-context_window(width::Integer, height::Integer; name="SciGL.jl", window_hints=default_window_hints) = GLFW.Window(name=name, resolution=(width, height), windowhints=window_hints, focus=true) |> set_context
+context_window(width::Integer, height::Integer; name = "SciGL.jl", window_hints = default_window_hints) = GLFW.Window(name = name, resolution = (width, height), windowhints = window_hints, focus = true) |> set_context!
 
 """
     enable_depth_stencil()
@@ -50,7 +50,7 @@ end
     set_clear_color(color)
 Set a color which is used for glClear(GL_COLOR_BUFFER_BIT), default is black
 """
-function set_clear_color(color::AbstractRGBA=RGBA(0, 0, 0, 0))
+function set_clear_color(color::AbstractRGBA = RGBA(0, 0, 0, 0))
     glClearColor(red(color), green(color), blue(color), alpha(color))
 end
 
