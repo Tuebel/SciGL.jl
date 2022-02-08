861ccbedd1e54b7f61c92022bc62ed1a8a65e716
Tim Redick <t.redick@irt.rwth-aachen.de>
2022-02-08 20:43:55 +0100

    Mapping OpenGL textures to CUDA

--- src/Cuda.jl
+++ src/Cuda.jl
@@ -3,11 +3,12 @@
 # All rights reserved.
 using CUDA
 using CUDA:
+    CU_GRAPHICS_REGISTER_FLAGS_NONE,
     CU_GRAPHICS_REGISTER_FLAGS_TEXTURE_GATHER,
+    CU_RESOURCE_TYPE_ARRAY,
     CUarray,
     cuGraphicsGLRegisterImage,
     cuGraphicsMapResources,
-    CU_GRAPHICS_REGISTER_FLAGS_NONE,
     CUgraphicsResource,
     cuGraphicsSubResourceGetMappedArray,
     Mem
@@ -15,11 +16,11 @@ using GLAbstraction
 using ModernGL
 
 """
-    cuda_array_ptr(tex)
+    gltex_to_cuda_ptr(id)
 Registers the texture or renderbuffer object specified by image for access by CUDA.
 A CUarray pointer to the resource is returned.
 """
-function cuda_array_ptr(tex::GLAbstraction.Texture{T,2}; readonly = false) where {T}
+function gltex_to_cuda_ptr(id::GLuint; readonly = false) where {T}
     resources = Ref{CUDA.CUgraphicsResource}()
     # For reading also works: 
     if readonly
@@ -28,7 +29,7 @@ function cuda_array_ptr(tex::GLAbstraction.Texture{T,2}; readonly = false) where
         flags = CU_GRAPHICS_REGISTER_FLAGS_NONE
     end
     # resource as reference
-    cuGraphicsGLRegisterImage(resources, tex.id, GL_TEXTURE_2D, flags)
+    cuGraphicsGLRegisterImage(resources, id, GL_TEXTURE_2D, flags)
     cuGraphicsMapResources(1, resources, C_NULL)
     cuarray_ptr = Ref{CUarray}()
     # resources dereferenced
@@ -71,7 +72,7 @@ Copy a 2D texture to the CUDA device memory.
 CuMatrix type can differ from texture type if you know what you are doing (e.g. Flaot32 instead of Gray{Float32})
 """
 function Base.unsafe_copyto!(dest::CuMatrix{T}, src::GLAbstraction.Texture{U,2}) where {T,U}
-    ptr = cuda_array_ptr(src)
+    ptr = gltex_to_cuda_ptr(src.id)
     Base.unsafe_copyto!(dest, ptr)
 end
 
@@ -82,6 +83,94 @@ CuMatrix type can differ from texture type if you know what you are doing (e.g. 
 Prefer the GLAbstraction version which requires the same type for both
 """
 function Base.unsafe_copyto!(dest::Matrix{T}, src::GLAbstraction.Texture{U,2}) where {T,U}
-    ptr = cuda_array_ptr(src)
+    ptr = gltex_to_cuda_ptr(src.id)
     Base.unsafe_copyto!(dest, ptr)
+end
+
+# Mapping instead of copying, hopefully a pull request will make this copy pointless
+
+"""
+    SciTextureArray{T,N}
+Basically a copy of the `CuTextureArray` from CUDA.jl but with an additional internal constructor which allows to pass an existing `ArrayBuffer`.
+"""
+mutable struct SciTextureArray{T,N}
+    buf::Mem.ArrayBuffer{T}
+    dims::Dims{N}
+    ctx::CuContext
+
+    @doc """
+        SciTextureArray{T,N}(undef, dims)
+    Construct an uninitialized texture array of `N` dimensions specified in the `dims`
+    tuple, with elements of type `T`. Use `Base.copyto!` to initialize this texture array,
+    or use constructors that take a non-texture array to do so automatically.
+    """
+    function SciTextureArray{T,N}(::UndefInitializer, dims::Dims{N}) where {T,N}
+        buf = Mem.alloc(Mem.Array{T}, dims)
+        t = new{T,N}(buf, dims, context())
+        finalizer(unsafe_destroy!, t)
+        return t
+    end
+
+    @doc """
+        SciTextureArray{T,N}(buf, dims)
+    Construct an uninitialized texture array of `N` dimensions specified in the `dims`
+    tuple, with elements of type `T`. Use `Base.copyto!` to initialize this texture array,
+    or use constructors that take a non-texture array to do so automatically.
+    """
+    function SciTextureArray(buf::Mem.ArrayBuffer{T,N}) where {T,N}
+        t = new{T,N}(buf, buf.dims, context())
+        finalizer(unsafe_destroy!, t)
+        return t
+    end
+end
+
+function unsafe_destroy!(t::SciTextureArray)
+    context!(t.ctx; skip_destroyed = true) do
+        Mem.free(t.buf)
+    end
+end
+
+Base.size(tm::SciTextureArray) = tm.dims
+Base.length(tm::SciTextureArray) = prod(size(tm))
+Base.eltype(::SciTextureArray{T,N}) where {T,N} = T
+Base.sizeof(tm::SciTextureArray) = sizeof(eltype(tm)) * length(tm)
+Base.pointer(t::SciTextureArray) = t.buf.ptr
+CUDA.memory_source(::SciTextureArray) = CUDA.ArrayMemory()
+
+function CUDA.CUDA_RESOURCE_DESC(texarr::SciTextureArray)
+    # FIXME: manual construction due to invalid padding (JuliaInterop/Clang.jl#238)
+    resDesc_ref = Ref((CU_RESOURCE_TYPE_ARRAY, # resType::CUresourcetype
+        pointer(texarr), # 1 x UInt64
+        ntuple(_ -> Int64(0), 15), # 15 x UInt64
+        UInt32(0)))
+    return resDesc_ref
+end
+
+CUDA.CuTexture(x::SciTextureArray{T,N}; kwargs...) where {T,N} =
+    CuTexture{T,N,typeof(x)}(x; kwargs...)
+
+
+"""
+    CuTexture(texture)
+Map an OpenGL Texture to a CUDA Texture using the type of the texture.
+Color types seem to cause problems with some Kernels.
+"""
+function CUDA.CuTexture(texture::GLAbstraction.Texture{T,N}) where {T,N}
+    ptr = SciGL.gltex_to_cuda_ptr(texture.id)
+    typed_ptr = Base.unsafe_convert(CuArrayPtr{T}, ptr)
+    array_buf = CUDA.Mem.ArrayBuffer{T,N}(typed_ptr, size(texture))
+    texture_array = SciGL.SciTextureArray(array_buf)
+    CUDA.CuTexture(texture_array)
+end
+
+"""
+    CuTexture(texture)
+Map an OpenGL Texture to a CUDA Texture with an explicit type conversion.
+"""
+function CUDA.CuTexture(::Type{T}, texture::GLAbstraction.Texture{U,N}) where {T,U,N}
+    ptr = SciGL.gltex_to_cuda_ptr(texture.id)
+    typed_ptr = Base.unsafe_convert(CuArrayPtr{T}, ptr)
+    array_buf = CUDA.Mem.ArrayBuffer{T,N}(typed_ptr, size(texture))
+    texture_array = SciGL.SciTextureArray(array_buf)
+    CUDA.CuTexture(texture_array)
 end
