9a8ce1a87ff7ad2fd963c0062985d79e73ef16e5
Tim Redick <t.redick@irt.rwth-aachen.de>
2023-01-06 23:10:07 +0100

    Check texture size when activating layer

--- src/Layers.jl
+++ src/Layers.jl
@@ -12,6 +12,10 @@ Does not for RBOs since they are inherently 2D.
 function activate_layer(framebuffer::GLAbstraction.FrameBuffer, layer::Int)
     # SciGL only uses the first attachment
     texture = first(GLAbstraction.color_attachments(framebuffer))
+    if layer > texture |> size |> last
+        @error "Cannot activate layer $layer, texture size $(size(texture))"
+        return
+    end
     # C starts counting at 0 → layer-1
     glFramebufferTextureLayer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, texture.id, 0, layer - 1)
 end
