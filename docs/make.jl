22d2a528132ad4c58de179ba2e2d8b1f4d9e0dc2
Tim Redick <tim.redick@rwth-aachen.de>
2024-04-23 17:31:25 +0200

    correct repo for docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,9 @@
 # @license BSD-3 https://opensource.org/licenses/BSD-3-Clause
-# Copyright (c) 2022, Institute of Automatic Control - RWTH Aachen University
+# Copyright (c) 2024, Institute of Automatic Control - RWTH Aachen University
 # All rights reserved. 
 
 using Documenter, SciGL
 import Documenter.Remotes: GitLab
 
-makedocs(sitename="SciGL.jl", repo=GitLab("git-ce.rwth-aachen.de", "diss", "scigl.jl"))
+makedocs(sitename="SciGL")
+deploydocs(repo="github.com/rwth-irt/SciGL.jl.git")
